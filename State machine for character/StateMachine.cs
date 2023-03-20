97e67650a276a5f4462681f19cf1a280a49efc48
jacob cyr <jacobncyr@outlook.com>
2023-03-20 12:54:45 -0600

    initial commit

---
+++ State machine for character/StateMachine.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+
+    private State currentState;
+
+    private void Update()
+    {
+        currentState?.Tick(Time.deltaTime);
+    }
+    void SwitchState(State newState){
+        currentState?.Exit();
+        currentState = newState;
+        currentState?.Enter();
+    }
+
+}
