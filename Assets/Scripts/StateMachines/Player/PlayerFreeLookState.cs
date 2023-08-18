0899412589c13f3d119ae974b87708ba23be4246
Jacob Cyr <jacobncyr@outlook.com>
2023-08-18 09:17:54 -0600

    fixed and broke stuff now understand fully

--- Assets/Scripts/StateMachines/Player/PlayerFreeLookState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerFreeLookState.cs
@@ -13,8 +13,6 @@ public class PlayerFreeLookState : PlayerBaseState
     public override void Enter()
     {
         stateMachine.InputReader.TargetEvent += OnTarget;
-
-
     }
 
     public override void Tick(float deltaTime)
@@ -38,10 +36,14 @@ public class PlayerFreeLookState : PlayerBaseState
     {
         stateMachine.InputReader.TargetEvent -= OnTarget;
     }
+
     private void OnTarget()
     {
+        if (!stateMachine.Targeter.SelectTarget()) { return; }
+
         stateMachine.SwitchState(new PlayerTargetingState(stateMachine));
     }
+
     private Vector3 CalculateMovement()
     {
         Vector3 forward = stateMachine.MainCameraTransform.forward;
--- Assets/Scripts/StateMachines/Player/PlayerTargetingState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerTargetingState.cs
@@ -1,34 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerTargetingState : PlayerBaseState
 {
-    public PlayerTargetingState(PlayerStateMachine stateMachine) : base(stateMachine)
-    {
-
-    }
+    public PlayerTargetingState(PlayerStateMachine stateMachine) : base(stateMachine) { }
 
     public override void Enter()
     {
         stateMachine.InputReader.CancelEvent += OnCancel;
     }
 
-    public override void Exit()
-    {
-        stateMachine.InputReader.CancelEvent += OnCancel;
-    }
-        
-    private void OnCancel()
-    {
-        stateMachine.SwitchState(new PlayerFreeLookState(stateMachine));
-    }
-        
     public override void Tick(float deltaTime)
     {
-        throw new System.NotImplementedException();
+        Debug.Log(stateMachine.Targeter.CurrentTarget.name);
     }
 
+    public override void Exit()
+    {
+        stateMachine.InputReader.CancelEvent -= OnCancel;
+    }
 
+    private void OnCancel()
+    {
+        stateMachine.Targeter.Cancel();
+
+        stateMachine.SwitchState(new PlayerFreeLookState(stateMachine));
+    }
 }
